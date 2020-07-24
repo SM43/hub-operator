e0c2b6baee8002a5b800f3f855df229c21d8ae8a
SM43 <smukhade@redhat.com>
2020-07-25 01:13:34 +0530

    New Operator

---
+++ pkg/apis/hub/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the hub v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=hub.tekton.dev
+package v1alpha1
---
+++ pkg/apis/hub/v1alpha1/hub_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HubSpec defines the desired state of Hub
+type HubSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// HubStatus defines the observed state of Hub
+type HubStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Hub is the Schema for the hubs API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=hubs,scope=Namespaced
+type Hub struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HubSpec   `json:"spec,omitempty"`
+	Status HubStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// HubList contains a list of Hub
+type HubList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Hub `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Hub{}, &HubList{})
+}
